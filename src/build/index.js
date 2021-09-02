cc8b60a286131016fa05a6518f380455fc44523d
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2021-09-02 12:48:50 +0100

    Add build script to create index file

---
+++ src/build/index.js
@@ -0,0 +1,23 @@
+// File paths assume that CWD is the root of the project
+const indexTmplPath = './src/templates/index.tmpl.html';
+
+console.log('Reading', indexTmplPath);
+const indexTmpl = await Deno.readTextFile(indexTmplPath);
+
+const includeMatches = indexTmpl.matchAll(/<include src="([./\w-]+.tmpl.html)">/g);
+
+let output = indexTmpl;
+
+for (const matchArray of includeMatches) {
+	const [match, path] = matchArray;
+	
+	console.log('Reading', path);
+	const includeTmpl = await Deno.readTextFile(path);
+
+	output = output.replace(match, includeTmpl);
+}
+
+const outputPath = './index.html';
+
+console.log('Writing', outputPath);
+await Deno.writeTextFile(outputPath, output);
