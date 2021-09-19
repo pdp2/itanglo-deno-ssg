2ffd30377677f9cd129041de72d7e51cd79380f6
Paolo Di Pasquale <paolo.dipasquale@yell.com>
2021-09-19 11:00:45 +0100

    Humour my indentation OCD

--- src/build/tests/markdownParser.test.js
+++ src/build/tests/markdownParser.test.js
@@ -4,41 +4,41 @@ import mdExamples from './stubs/markdownExamples.js';
 
 
 Deno.test('should parse an h1 heading', () => {
-  const h1 = mdParser.parse(mdExamples.h1Md);
-  assertEquals(h1, mdExamples.h1Html);
+    const h1 = mdParser.parse(mdExamples.h1Md);
+    assertEquals(h1, mdExamples.h1Html);
 });
 
 Deno.test('should parse an h2 heading', () => {
-  const h2 = mdParser.parse(mdExamples.h2Md);
-  assertEquals(h2, mdExamples.h2Html);
+    const h2 = mdParser.parse(mdExamples.h2Md);
+    assertEquals(h2, mdExamples.h2Html);
 });
 
 Deno.test('should parse an h3 heading', () => {
-  const h3 = mdParser.parse(mdExamples.h3Md);
-  assertEquals(h3, mdExamples.h3Html);
+    const h3 = mdParser.parse(mdExamples.h3Md);
+    assertEquals(h3, mdExamples.h3Html);
 });
 
 Deno.test('should parse a paragraph', () => {
-  const p = mdParser.parse(mdExamples.pMd);
-  assertEquals(p, mdExamples.pHtml);
+    const p = mdParser.parse(mdExamples.pMd);
+    assertEquals(p, mdExamples.pHtml);
 });
 
 Deno.test('should parse a link', () => {
-  const a = mdParser.parse(mdExamples.aMd);
-  assertEquals(a, mdExamples.aHtml);
+    const a = mdParser.parse(mdExamples.aMd);
+    assertEquals(a, mdExamples.aHtml);
 });
 
 Deno.test('should parse inline code', () => {
-  const code = mdParser.parse(mdExamples.codeMd);
-  assertEquals(code, mdExamples.codeHtml);
+    const code = mdParser.parse(mdExamples.codeMd);
+    assertEquals(code, mdExamples.codeHtml);
 });
 
 Deno.test('should parse a code block', () => {
-  const codeBlock = mdParser.parse(mdExamples.codeBlockMd);
-  assertEquals(codeBlock, mdExamples.codeBlockHtml);
+    const codeBlock = mdParser.parse(mdExamples.codeBlockMd);
+    assertEquals(codeBlock, mdExamples.codeBlockHtml);
 });
 
 Deno.test('should parse an image', () => {
-  const img = mdParser.parse(mdExamples.imgMd);
-  assertEquals(img, mdExamples.imgHtml);
+    const img = mdParser.parse(mdExamples.imgMd);
+    assertEquals(img, mdExamples.imgHtml);
 });
